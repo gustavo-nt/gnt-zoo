0c4b0951e09722f5378a841f1de5def17aadfb69
gustavo-nt <gustavont.dev@gmail.com>
2022-01-27 21:27:56 -0300

    Add prettier and formatter swipper.js

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,10 +1,10 @@
-const containerInput = document.querySelector('.search');
-const inputText = document.querySelector('.search input');
+const containerInput = document.querySelector(".search");
+const inputText = document.querySelector(".search input");
 
-inputText.onblur = function() {
-    if (inputText.value !== '') {
-        containerInput.classList.add('active');
-    } else {
-        containerInput.classList.remove('active');
-    }
-}
+inputText.onblur = function () {
+  if (inputText.value !== "") {
+    containerInput.classList.add("active");
+  } else {
+    containerInput.classList.remove("active");
+  }
+};
--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,28 +1,32 @@
-const arrowLeft = document.querySelector('.arrowleft');
-const arrowRight = document.querySelector('.arrowright');
-const rowList = document.querySelector('.rowlist');
+const arrowLeft = document.querySelector(".arrowleft");
+const arrowRight = document.querySelector(".arrowright");
+const rowList = document.querySelector(".rowlist");
 const style = getComputedStyle(rowList);
 
-arrowLeft.onclick = function() {
-    let x = parseInt(style.marginLeft.replace('px', '')) + Math.round(window.innerWidth / 2);
+arrowLeft.onclick = function () {
+  let x =
+    parseInt(style.marginLeft.replace("px", "")) +
+    Math.round(window.innerWidth / 2);
 
-    if (x > 0) {
-        x = 0;
-    }
-    rowList.style.marginLeft = `${x}px`;
-}
+  if (x > 0) {
+    x = 0;
+  }
+  rowList.style.marginLeft = `${x}px`;
+};
 
-arrowRight.onclick = function() {
-    let x = parseInt(style.marginLeft.replace('px', '')) - Math.round(window.innerWidth / 2); 
+arrowRight.onclick = function () {
+  let x =
+    parseInt(style.marginLeft.replace("px", "")) -
+    Math.round(window.innerWidth / 2);
 
-    if((window.innerWidth - 765) > x) {
-        x = (window.innerWidth - 765) - (window.innerWidth * 3.5 / 100);
-    }
-    rowList.style.marginLeft = `${x}px`;
-}
+  if (window.innerWidth - 765 > x) {
+    x = window.innerWidth - 765 - (window.innerWidth * 3.5) / 100;
+  }
+  rowList.style.marginLeft = `${x}px`;
+};
 
-window.addEventListener('resize', function(e){
-    if (window.innerWidth > 768) {
-        rowList.style.marginLeft = '0px';
-    }
-}); 
+window.addEventListener("resize", function (e) {
+  if (window.innerWidth > 768) {
+    rowList.style.marginLeft = "0px";
+  }
+});
